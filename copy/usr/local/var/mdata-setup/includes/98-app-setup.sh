c1659d3f63cddc41b34d9decc0cbd5eba76c8e3b
jfqd <jfqd@blun.org>
2023-01-24 15:30:56 +0100

    add configuration

--- copy/usr/local/var/mdata-setup/includes/98-app-setup.sh
+++ copy/usr/local/var/mdata-setup/includes/98-app-setup.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+MAIL_UID=$(/native/usr/sbin/mdata-get mail_auth_user)
+MAIL_PWD=$(/native/usr/sbin/mdata-get mail_auth_pass)
+MAIL_HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
+
+DOMAIN=$(/native/usr/sbin/mdata-get peertube_domain)
+ADMIN_EMAIL=$(/native/usr/sbin/mdata-get admin_email)
+FROM_EMAIL=$(/native/usr/sbin/mdata-get from_email)
+SECRET=$(openssl rand -hex 32)
+
+sed -i \
+    -e "s/hostname: 'example.com'/hostname: '${DOMAIN}'/" \
+    -e "s/peertube: ''/peertube: '${SECRET}'/" \
+    -e "s/email: 'admin@example.com'/email: '${ADMIN_EMAIL}'/" \
+    -e "s/from_address: 'admin@example.com'/from_address: '${FROM_EMAIL}'/" \
+    /var/www/peertube/config/production.yaml
+
 echo "* Setup postgresql backup"
 mkdir -p /var/lib/postgresql/backups
 chown postgres:postgres /var/lib/postgresql/backups
@@ -8,9 +24,10 @@ echo "0 2 1 * * /usr/bin/vacuumdb --all" >> /var/spool/cron/crontabs/postgres
 chown postgres:crontab /var/spool/cron/crontabs/postgres
 chmod 0600 /var/spool/cron/crontabs/postgres
 
-# sed -i 's/${WEBSERVER_HOST}/[peertube-domain]/g' /etc/nginx/sites-available/peertube
-# sed -i 's/${PEERTUBE_HOST}/127.0.0.1:9000/g' /etc/nginx/sites-available/peertube
+sed -i 's/WEBSERVER_HOST/${DOMAIN}/g' /etc/nginx/sites-available/peertube
 
+systemctl enable peertube || true
+systemctl start peertube || true
 systemctl restart nginx || true
 
 exit 0
